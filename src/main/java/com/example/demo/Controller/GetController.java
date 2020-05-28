c664c9424328d4e42a2bb7b7db8a1ed9010363a1
jackytsheng <jackytsheng@gmail.com>
2020-05-29 00:13:58 +1000

    new project to try out REST and JWT myself without tutorial

--- src/main/java/com/example/demo/Controller/GetController.java
+++ src/main/java/com/example/demo/Controller/GetController.java
@@ -1,14 +1,28 @@
 package com.example.demo.Controller;
 
 
-import org.springframework.stereotype.Controller;
+import com.example.demo.Student;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
-@Controller
+@RestController
 public class GetController {
 
-  @GetMapping('students')
+  @GetMapping("students")
+  ResponseEntity<ArrayList<Student>> all(){
+    Student Student1 = new Student("Jacky",1,"Student");
+    Student Student2 = new Student("Jacky2",2,"Student");
+    ArrayList<Student> StudentList = new ArrayList<>();
+    StudentList.add(Student1);
+    StudentList.add(Student2);
 
+    return new ResponseEntity<>(StudentList, HttpStatus.OK);
+  }
 
 }
